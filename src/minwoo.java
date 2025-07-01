01221dd8b63e2c14dbdead1326ada4cd8049ddd9
Min <kineuns@gmail.com>
2025-07-01 15:59:42 +0900

    0701

--- src/minwoo.java
+++ src/minwoo.java
@@ -4,7 +4,7 @@ public class minwoo {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf(" welcome!");
+        System.out.println(" welcome!");
 
         for (int i = 1; i <= 6; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
